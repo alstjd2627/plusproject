4244fb25a2c48ba13b2268739ad543fba46bb4ef
minseong1 <alstjd2627@naver.com>
2023-12-22 16:24:40 +0900

    init commit

---
+++ src/main/java/com/sparta/plusproject/PlusprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.plusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusprojectApplication.class, args);
+    }
+
+}
